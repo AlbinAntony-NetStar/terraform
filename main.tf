308908393e1e755726c3fe1ca9161a9e7794781a
Albin Antony <albin@example.com>
2022-02-09 02:38:08 +0530

    test2

--- main.tf
+++ main.tf
@@ -9,3 +9,46 @@ terraform {
 provider "aws" {
   region = "${var.aws-region}"
 }
+
+
+resource "aws_instance" "terraform" {
+  ami           = "ami-055d15d9cfddf7bd3"
+  security_groups = ["${aws_security_group.web-sg.name}"]
+  instance_type = "t2.micro"
+  key_name 		= "demokey"
+
+
+resource "aws_security_group" "web-sg" {
+  name        = "webserver-sg"
+  description = "Security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
